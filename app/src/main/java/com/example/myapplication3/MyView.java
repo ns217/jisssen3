8860676f26efcee615db85d024595211ca7efc3b
ns217 <zsatai499@gmail.com>
2025-05-07 09:50:26 +0900

    commit

--- app/src/main/java/com/example/myapplication3/MyView.java
+++ app/src/main/java/com/example/myapplication3/MyView.java
@@ -1,28 +1,35 @@
 package com.example.myapplication3;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MyView extends View {
     public MyView(Context context) {
         super(context);
     }
 
-
-
+    public MyView(Context context, @Nullable AttributeSet attrs){
+        super(context,attrs);
+    }
 
 
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(@NonNull Canvas canvas){
         super.onDraw(canvas);
 
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
+        @SuppressLint("DrawAllocation") Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        p.setStrokeWidth(10);
 
-        canvas.drawLine(10,20,30,40,paint);
+        canvas.drawLine(10,20,30,40,p);
     }
 
 }
