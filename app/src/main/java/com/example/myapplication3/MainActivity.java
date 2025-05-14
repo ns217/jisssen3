0828ee8dfeda8e903bcb3c2703919cf493c7cca6
ns217 <zsatai499@gmail.com>
2025-05-14 16:26:12 +0900

    commit

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -1,58 +1,60 @@
 package com.example.myapplication3;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import java.util.List;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity implements LocationListener {
 
-    private SensorManager manager;
+    private LocationManager manager;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        manager = (LocationManager)getSystemService(LOCATION_SERVICE);
     }
 
     public void  onResume() {
         super.onResume();
 
-        List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_LIGHT);
-
-        if (sensors.size() != 0){
-            Sensor sensor = sensors.get(0);
-            manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,10,this);
 
         }
-    }
 
-    protected  void  onPause(){
+    protected void onPause(){
         super.onPause();
-        manager.unregisterListener(this);
+        manager.removeUpdates(this);
     }
 
     @Override
-    public void onAccuracyChanged(Sensor arg0, int arg1) {
-
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Toast.makeText(this,String.format("%.3f %.3f", lat,lng),Toast.LENGTH_SHORT).show();
     }
-
-    @Override
-    public void onSensorChanged(SensorEvent arg0) {
-        if (arg0.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            float x = arg0.values[0];
-            float y = arg0.values[1];
-            float z = arg0.values[2];
-            String str = Float.toString(x)+","+ Float.toString(y)+","+ Float.toString(z);
-            TextView textView = (TextView) findViewById(R.id.status_text);
-            textView.setText(str);
-        }
-
-    }
-
 }
